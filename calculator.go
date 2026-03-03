3c71582066cea30ca31ab618607af52f1f329bd8
Aleksandra Kau-Ten-Chzhi <alexandra@childish.eu>
2026-03-03 20:46:32 +0200

    Calculations draft

--- calculator.go
+++ calculator.go
@@ -15,16 +15,34 @@ type IncomeForm struct {
 	TaxesConfig TaxesConfig
 }
 
-func CalculateSocialSecurity() {
-	// TODO
+func CalculateSocialSecurity(f IncomeForm, settings Settings) int32 {
 	// За да определите осигуровките си
-	// следва да умножите осигурителен доход (макс по дефолт) по 0.278 или 0.313
+	// следва да умножите осигурителен доход по 0.278 или 0.313
 	// в зависимост от това дали имате включено майчинство в настройките.
+
+	insurancePercent := f.TaxesConfig.PensionPercentage + f.TaxesConfig.HealthInsurancePercentage
+	if settings.IsPregnancyInsuranceEnabled {
+		insurancePercent += f.TaxesConfig.PregnancyInsurancePercentage
+	}
+
+	res := float32(f.TaxedIncomeCents) * insurancePercent / 100
+
+	return int32(res)
 }
 
-func CalculateAdvanceTaxForThreeMonths() {
-	// TODO
-	//За да определите авансовия си данък за тримесечието
-	//следва да извадите от доход за 3 месеца признатите разходи 25%
-	//и платените осигуровки за трите месеца. Данък 10%
+func CalculateTaxForMonth(f IncomeForm, insurance int32) int32 {
+	// За да определите авансовия си данък
+	// следва да извадите от доход  признатите разходи 25%
+	// и платените осигуровки
+
+	expenses := int32(float32(f.MonthIncomeCents) * f.TaxesConfig.ExpensesPercentage / 100)
+
+	return int32(f.MonthIncomeCents) - expenses - insurance
+}
+
+func CalculateAdvanceTaxForThreeMonths(f IncomeForm, insurance int32) int32 {
+	// За да определите авансовия си данък за тримесечието
+	// следва да извадите от доход за 3 месеца признатите разходи 25%
+	// и платените осигуровки за трите месеца. Данък 10%
+	return 0 // TODO
 }
